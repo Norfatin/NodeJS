4deb5e69baf990f71b398de474354a311188b1b4
Norfatin <44288062+Norfatin@users.noreply.github.com>
2020-10-05 14:33:11 +0800

    finished section5

--- Section5_expressjs/assignment2/app.js
+++ Section5_expressjs/assignment2/app.js
@@ -1,16 +1,15 @@
+const path = require("path");
+
 const http = require("http");
 
 const express = require("express");
 
-const app = express();
-app.use("/users", (req, res, next) => {
-  console.log("second middleware");
-  res.send("<h1>Users page for Assignment2</h1>");
-});
+const mainRoutes = require("./routes/index.js");
 
-app.use("/", (req, res, next) => {
-  console.log("/ middleware");
-  res.send("<h1>Assignment2 page</h1>");
-});
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(mainRoutes);
 
 app.listen(3000);
